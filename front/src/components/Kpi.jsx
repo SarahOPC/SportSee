c80500b4c0cfc019c051ae7a0970e4060579141d
Sarah Raguin <sarahraguin@gmail.com>
2023-08-22 08:02:05 -0400

    Working and trying to solve the bug on graph with recharts

--- front/src/components/Kpi.jsx
+++ front/src/components/Kpi.jsx
@@ -1,5 +1,5 @@
 // import { fetchUserData } from '../DataFetchingFile';
-import { RadialBarChart, RadialBar, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+/* import { RadialBarChart, RadialBar, Legend, Tooltip, ResponsiveContainer } from 'recharts';
 import { useState, useEffect } from 'react';
 import { fetchMockedUserData } from '../DataFetchingFile';
 import { formatUserData } from './DataModeling';
@@ -25,22 +25,59 @@ function Kpi() {
         return <div>Loading...</div>;
     }
     
-    const chartData = [{ formattedScore: userScore.formattedScore }];
-    console.log(chartData[0].formattedScore);
+    const chartData = [{ name: 'Score', value: userScore.formattedScore }];
+    console.log(chartData);
 
     return(
         <ResponsiveContainer width="100%" height="100%">
-            <RadialBarChart  cx="50%" cy="50%" innerRadius="10%" outerRadius="80%" startAngle={180} endAngle={0} data={chartData[0]}>
+
+            <RadialBarChart innerRadius="10%" outerRadius="80%" data={chartData}>
                 <RadialBar 
                     minAngle={15} 
                     label={{ fill: '#666', position: 'insideStart' }} 
                     background 
                     clockWise={true} 
-                    dataKey="formattedScore" />
+                    dataKey="value" />
                 <Legend iconSize={10} width={120} height={140} layout='vertical' verticalAlign='middle' align="right" />
                 <Tooltip />  
             </RadialBarChart>
         </ResponsiveContainer>
+    );
+}
+
+export default Kpi; */
+
+
+
+import { RadialBarChart, RadialBar } from 'recharts';
+
+function Kpi() {
+    const data = [
+        { name: 'Score', value: 47 }
+    ];
+
+    return (
+            <div style={{ textAlign: 'center' }}>
+                <h1>Hello, Recharts!</h1>
+                <RadialBarChart data={data}>
+                    <RadialBar
+                        /* minAngle={15}
+                        label={{ fill: '#666', position: 'insideStart' }}
+                        background
+                        clockWise={true} */
+                        dataKey="value"
+                    />
+                    {/* <Legend
+                        iconSize={10}
+                        width={120}
+                        height={140}
+                        layout="vertical"
+                        verticalAlign="middle"
+                        align="right"
+                    />
+                    <Tooltip /> */}
+                </RadialBarChart>
+            </div>
     );
 }
 
