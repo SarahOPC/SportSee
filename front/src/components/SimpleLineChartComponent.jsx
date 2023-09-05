cc53e1c7d560315eb98319fce25a688357130eb4
Sarah Raguin <sarahraguin@gmail.com>
2023-09-05 09:37:28 -0400

    Adding style to the simple line chart 'Duree moyenne des sessions'

--- front/src/components/SimpleLineChartComponent.jsx
+++ front/src/components/SimpleLineChartComponent.jsx
@@ -1,30 +1,92 @@
 import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { useState } from 'react';
 
-const renderLineChart = (data) => ( // Accept data prop as a parameter
-  <div width={400} height={350}>
+const CustomLegend = (props) => {
+  const whiteColor = "#FFFFFF";
+  const legendStyle = {
+    color: whiteColor,
+    textAlign: 'left',
+    opacity: '0.6',
+    fontSize: '1.3em',
+    marginLeft: '4em',
+  };
+
+  return (
+    <div>
+      <div style={legendStyle}>Durée moyenne des sessions</div>
+    </div>
+  );
+};
+
+const tooltipStyle = {
+  backgroundColor: '#FFFFFF',
+  padding: '0.5em',
+  fontWeight: '600'
+};
+
+const dayOfWeek = ['L','M','M','J','V','S','D'];
+
+const CustomTooltip = ({ active, payload }) => {
+  if(active && payload && payload.length) {
+    return (
+      <div style={tooltipStyle}>{`${payload[0].value} min`}</div>
+    )
+  };
+  return null;
+}
+
+const RenderLineChart = ({data}) => { // Accept data prop as a parameter
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <div onMouseEnter= {() => setHovered(true)}
+    onMouseLeave={() => setHovered(false)}
+    style={{ position: 'relative' }}>
+    
+    {hovered && (
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: '50%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        transform: 'translateX(-50%)',
+        zIndex: -1,
+      }}
+      ></div>
+    )}
 
     <LineChart
       width={258}
-      height={263}
+      height={275}
       data={data}
       margin={{
-        top: 35,
-        right: 30,
-        left: 10,
+        top: 15,
+        right: 0,
+        left: -65,
         bottom: 15,
       }}
-    >
+      >
+      <defs>
+        <linearGradient id='gradient' x1="0" y1="0" x2="100%" y2="0" >
+          <stop offset="0%" stopColor='#BFBFBF' />
+          <stop offset="100%" stopColor='#FFFFFF' />
+        </linearGradient>
+      </defs>
+      <Legend content={CustomLegend} verticalAlign='top'/>
       <rect width="100%" height="100%" fill="#FF0000" />
-      <XAxis dataKey="day" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line type="monotone" dataKey="sessionLength" stroke="#FFFFFF" dot={false} strokeWidth={1.5} />
+      <XAxis dataKey="day" axisLine={false} tickLine={false}
+      tickFormatter={(value) => dayOfWeek[value - 1]} tick={{fill: '#FFFFFF', opacity: 0.6}} padding={{left: 10, right: 10}} />
+      <YAxis axisLine={false} tick={null} />
+      <Tooltip content={CustomTooltip} cursor={false} />
+      <Line type="monotone" dataKey="sessionLength" stroke="url(#gradient)" dot={false} strokeWidth={1.5} />
     </LineChart>
 
   </div>
-)
+  );
+};
 
 export default function SimpleLineChartFunction({ data }) {
-    return renderLineChart(data); // Pass the data prop to renderLineChart
+    return <RenderLineChart data={data} />; // Pass the data prop to renderLineChart
 }
